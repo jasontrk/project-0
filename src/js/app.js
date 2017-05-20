453c409b8ece637b0fd631eecb572fef2ba1f3fd
Jason Ranjitkumar <jasontrk@hotmail.co.uk>
2017-05-20 18:18:24 +0100

    day1_shuffling_letters

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,20 @@
 console.log('JS Loaded');
+// const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+//   'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+//   't', 'u', 'v', 'w', 'x', 'y', 'z'];
+//
+// const movie1 = 'starwars'
+
+const shuffle = function () {
+  var x = this.split(''),
+    n = x.length;
+
+  for(var i = n - 1; i > 0; i--) {
+    var y = Math.floor(Math.random() * (i + 1));
+    var tmp = x[i];
+    x[i] = x[y];
+    x[y] = tmp;
+  }
+  return x.join('');
+};
+console.log('test'.shuffle());
