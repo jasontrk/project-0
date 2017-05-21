1671e175da4f58bddae91489d59171a260d146bf
Jason Ranjitkumar <jasontrk@hotmail.co.uk>
2017-05-21 22:37:04 +0100

    added shuffle function

--- src/js/app.js
+++ src/js/app.js
@@ -1,20 +1,57 @@
 console.log('JS Loaded');
-// const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-//   'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-//   't', 'u', 'v', 'w', 'x', 'y', 'z'];
-//
-// const movie1 = 'starwars'
 
-const shuffle = function () {
-  var x = this.split(''),
-    n = x.length;
+
+// Outside Shuffle function:
+//
+//          //create array before shuffle
+//     movietitle = title.split('');
+//     //in jquery
+//     i = 0;
+//     //ensures the correct number of boxes
+//     boxes.each(function(){
+//          $(this).data('letter', movietitle[i]);
+//          i++;
+//     });
+
+
+String.prototype.shuffle = function () {
+  var a = this.split(''),
+    n = a.length;
 
   for(var i = n - 1; i > 0; i--) {
-    var y = Math.floor(Math.random() * (i + 1));
-    var tmp = x[i];
-    x[i] = x[y];
-    x[y] = tmp;
+    var j = Math.floor(Math.random() * (i + 1));
+    var tmp = a[i];
+    a[i] = a[j];
+    a[j] = tmp;
   }
-  return x.join('');
+  return a.join('');
 };
-console.log('test'.shuffle());
+
+//tests
+console.log('STAR WARS'.shuffle());
+
+
+console.log('PREDATOR'.shuffle());
+
+
+// Outside Shuffle function:
+
+    //      //here you'll need to create the multidimensional array before shuffle him
+    // movietitle = title.split("");
+    // //in jquery
+    // i = 0;
+    // //certify you will have a right number of boxes
+    // boxes.each(function(){
+    //      $(this).data('letter', movietitle[i]);
+    //      i++;
+    // });
+
+//-> "movietitleshuffled
+
+
+//Action  Plan:
+// Create basic layout containing 12 divs. 1 main container. 1 header containing 3 headers for logo, timer and score. 1 'top' div containing 3 divs for movie picture, movie title result, game options. 1 'bottom' div containing a letters div with an unordered letters list within.
+
+// Work on the shuffle function and figure out how to make each shuffled letter clickable as well as make it dissapear and show in the 'movie title' div. The letters will be placed in order of clicks and can only progress to next word once in the right order. Use the splited array with a forEach statement when creating the boxes.
+
+//Stackoverflow: You don't need to hide the letters, use data-attributes to identify the right position of them in the Movie Title container and append that in the empty boxes. The letters that doesn't correspond to the movie title just put with data-attributes empty and your algorithm will filter that putting a red border into selected wrong letter and stuff;
