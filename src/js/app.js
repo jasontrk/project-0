01417b74e6df43691c09c406f5f5b2e2d5842908
Jason Ranjitkumar <jasontrk@hotmail.co.uk>
2017-05-25 14:54:49 +0100

    welcome page, game over and restart all working

--- src/js/app.js
+++ src/js/app.js
@@ -1,197 +1,189 @@
 $(() => {
   console.log('Guess Movie Game');
 
+  const audio = $('audio')[0];
+  const $result = $('#result_list');
+  const $letters = $('#letters_list');
+  const $hint = $('#dropdownHint');
+  const movieTitles = [{
+    title: 'SCREAM',
+    image: 'scream.bmp',
+    audio: 'scream',
+    hint: 'What\'s your favourite Scary Movie?'
 
+  }, {
+    title: 'IRONMAN',
+    image: 'ironman.bmp',
+    audio: 'ironman',
+    hint: 'Heroes aren\'t born. They\'re built.'
+
+  }, {
+    title: 'PREDATOR',
+    image: 'predator.bmp',
+    audio: 'predator',
+    hint: 'Soon the hunt will begin.'
+  }, {
+    title: 'ROBOCOP',
+    image: 'robocop.bmp',
+    audio: 'robocop',
+    hint: 'Part man. Part machine. All cop.'
+  }, {
+    title: 'BATMAN_VS_SUPERMAN',
+    image: 'batmanvssuperman.bmp',
+    audio: 'batmanvssuperman',
+    hint: 'Dawn of Justice'
+  }, {
+    title: 'GLADIATOR',
+    image: 'gladiator.bmp',
+    audio: 'gladiator',
+    hint: 'Father of a murdered son, husband to a murdered wife and I shall have my vengeance in this life or the next.'
+  }, {
+    title: 'FRIDAY_THE_13TH',
+    image: 'fridaythe13th.bmp',
+    audio: 'fridaythe13th',
+    hint: 'Fridays will never be the same again.'
+  }, {
+    title: 'V_FOR_VENDETTA',
+    image: 'vforvendetta.bmp',
+    audio: 'vforvendetta',
+    hint: 'Remember, remember the 5th of November.'
+  }, {
+    title: 'PHANTOM_OF_THE_OPERA',
+    image: 'phantomoftheopera.bmp',
+    audio: 'phantomoftheopera'
+  }];
+
+  let currentIndex = 0;
+  let currentTitle = movieTitles[currentIndex];
+  let timerId = null;
+
+  function progress(timeleft, timetotal, $element) {
+    clearTimeout(timerId);
+    var progressBarWidth = timeleft * $element.width() / timetotal;
+    $element.find('#bar').animate({ width: progressBarWidth }, 500).html(timeleft + 's');
+    if(timeleft > 0) {
+      timerId = setTimeout(function() {
+        progress(timeleft - 1, timetotal, $element);
+      }, 1000);
+    }
+    if(timeleft === 0){
+      console.log(audio);
+      $('.gameRunning').hide();
+      audio.pause();
+      $('.gameover').fadeIn();
+      clearLetters();
+    }
+  }
+
+  console.log('Movie:', currentTitle);
+
+  function congrats() {
+    $letters.html('');
+    $result.html('Nice. <br>On to the next one.');
+
+  }
+  function clearLetters() {
+    $letters.html('');
+    $result.html('');
+  }
+
+
+  // shuffle function
+  String.prototype.shuffle = function () {
+    var a = this.split(''),
+      n = a.length;
+
+    for(var i = n - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var tmp = a[i];
+      a[i] = a[j];
+      a[j] = tmp;
+    }
+    return a.join('');
+  };
+
+
+  // shuffled word and place in letters
+  function displayWord(){
+    const shuffledWord = currentTitle.title.shuffle();
+    $letters.empty();
+    // console.log('shuffled word', shuffledWord);
+    for (let i=0; i < shuffledWord.length; i++ ) {
+      $letters.append(`<li data-index=${i}>${shuffledWord[i]}</li>`);
+    }
+    progress(10, 10, $('#progressBar'));
+
+    // image change when movie title changes
+    $('#movie_picture').css('background-image', `url(public/assets/${currentTitle.image})`);
+    // audio change when movie title changes
+    audio.src = `public/assets/${currentTitle.audio}.mp3`;
+    audio.loop = true;
+    audio.play();
+    // // hint changes when movie title changes
+    // $('#dropdownHint').html;
+    $hint.text(currentTitle.hint);
+  }
+
+// start on welcome screen. click button to hide welcome page and run game
   function hideWelcome() {
     $('.welcome').fadeOut();
-    runGame();
-  }
-
-  $('#welcomeBtn').on('click', hideWelcome);
-  $('#restartBtn').on('click', runGame);
-
-
-
-  function runGame() {
-
-    let timerId = null;
-
-    function progress(timeleft, timetotal, $element) {
-      clearTimeout(timerId);
-      var progressBarWidth = timeleft * $element.width() / timetotal;
-      $element.find('#bar').animate({ width: progressBarWidth }, 500).html(timeleft + 's');
-      if(timeleft > 0) {
-        timerId = setTimeout(function() {
-          progress(timeleft - 1, timetotal, $element);
-        }, 1000);
-      }
-      if(timeleft === 0){
-        $('.gameRunning').hide();
-        $('.gameover').fadeIn();
-      }
-    }
-
-
-    const audio = $('audio')[0];
-    const $result = $('#result_list');
-    const $letters = $('#letters_list');
-    const $hint = $('#dropdownHint');
-
-
-    const movieTitles = [{
-      title: 'SCREAM',
-      image: 'scream.bmp',
-      audio: 'scream',
-      hint: 'What\'s your favourite Scary Movie?'
-
-    }, {
-      title: 'IRONMAN',
-      image: 'ironman.bmp',
-      audio: 'ironman',
-      hint: 'Heroes aren\'t born. They\'re built.'
-
-    }, {
-      title: 'PREDATOR',
-      image: 'predator.bmp',
-      audio: 'predator',
-      hint: 'Soon the hunt will begin.'
-    }, {
-      title: 'ROBOCOP',
-      image: 'robocop.bmp',
-      audio: 'robocop',
-      hint: 'Part man. Part machine. All cop.'
-    }, {
-      title: 'BATMAN_VS_SUPERMAN',
-      image: 'batmanvssuperman.bmp',
-      audio: 'batmanvssuperman',
-      hint: 'Dawn of Justice'
-    }, {
-      title: 'GLADIATOR',
-      image: 'gladiator.bmp',
-      audio: 'gladiator',
-      hint: 'Father of a murdered son, husband to a murdered wife and I shall have my vengeance in this life or the next.'
-    }, {
-      title: 'FRIDAY_THE_13TH',
-      image: 'fridaythe13th.bmp',
-      audio: 'fridaythe13th',
-      hint: 'Fridays will never be the same again.'
-    }, {
-      title: 'V_FOR_VENDETTA',
-      image: 'vforvendetta.bmp',
-      audio: 'vforvendetta',
-      hint: 'Remember, remember the 5th of November.'
-    }, {
-      title: 'PHANTOM_OF_THE_OPERA',
-      image: 'phantomoftheopera.bmp',
-      audio: 'phantomoftheopera'
-    }];
-    let currentIndex = 0;
-    let currentTitle = movieTitles[currentIndex];
-    console.log('Movie:', currentTitle);
-
-    function congrats() {
-      $letters.html('');
-      $result.html('Nice. <br>On to the next one.');
-
-    }
-    function clearLetters() {
-      $letters.html('');
-      $result.html('');
-      displayWord();
-    }
-
-    // shuffle function
-    String.prototype.shuffle = function () {
-      var a = this.split(''),
-        n = a.length;
-
-      for(var i = n - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var tmp = a[i];
-        a[i] = a[j];
-        a[j] = tmp;
-      }
-      return a.join('');
-    };
-
-
-    // shuffled word and place in letters
-    function displayWord(){
-      const shuffledWord = currentTitle.title.shuffle();
-      $letters.empty();
-      // console.log('shuffled word', shuffledWord);
-      for (let i=0; i < shuffledWord.length; i++ ) {
-        $letters.append(`<li data-index=${i}>${shuffledWord[i]}</li>`);
-      }
-      progress(10, 10, $('#progressBar'));
-
-      // image change when movie title changes
-      $('#movie_picture').css('background-image', `url(public/assets/${currentTitle.image})`);
-      // audio change when movie title changes
-      audio.src = `public/assets/${currentTitle.audio}.mp3`;
-      audio.loop = true;
-      audio.play();
-      // // hint changes when movie title changes
-      // $('#dropdownHint').html;
-      $hint.text(currentTitle.hint);
-
-
-    }
-
     displayWord();
+  }
+  $('#welcomeBtn').on('click', hideWelcome);
 
-    // hiding letters when clicked in the letters_list and show in result_list
-    $letters.on('click','li', (e) => {
-      $(e.target).hide();
-      const letter = $(e.target).text();
-      const index = $(e.target).data('index');
-      console.log(letter);
-      $result.append(`<li data-index=${index}>${letter}</li>`);
-      if ($result.text() === currentTitle.title) {
-        clearTimeout(timerId);
-        audio.src = `public/assets/correct.mp3`;
-        audio.loop = false;
-        audio.play();
-        congrats();
+  //restart game
+  function restartGame(){
+    $('.gameover').hide();
+    $('.gameRunning').show();
+    displayWord();
+  }
+  $('#restartBtn').on('click', restartGame);
+
+  // hiding letters when clicked in the letters_list and show in result_list
+  $letters.on('click','li', (e) => {
+    $(e.target).hide();
+    const letter = $(e.target).text();
+    const index = $(e.target).data('index');
+    console.log(letter);
+    $result.append(`<li data-index=${index}>${letter}</li>`);
+    if ($result.text() === currentTitle.title) {
+      clearTimeout(timerId);
+      audio.src = `public/assets/correct.mp3`;
+      audio.loop = false;
+      audio.play();
+      congrats();
 
 
-        if (currentIndex < movieTitles.length - 1) {
-          currentIndex++;
-          currentTitle = movieTitles[currentIndex];
-          setTimeout(clearLetters, 1800);
-        } else {
-          // game over logic
-        }
-
-
-        // console.log(clearLetters());
-        // return currentTitle.displayWord
-
+      if (currentIndex < movieTitles.length - 1) {
+        currentIndex++;
+        currentTitle = movieTitles[currentIndex];
+        clearLetters();
+        setTimeout(displayWord, 1800);
+      } else {
+        // game over logic
       }
 
 
-    });
+      // console.log(clearLetters());
+      // return currentTitle.displayWord
 
-    // hiding letters when clicked in result_list and show in letters_list
-    $result.on('click', 'li', (e) => {
-      const dataIndex = $(e.target).data('index');
-      $letters.children().filter((index) => {
-        return index === dataIndex;
-      }).show();
-      $(e.target).remove();
-
-      // use the data index to display the particular LI
-      // display none the LI we just clicked to move back down
-      console.log($(e.target));
-    });
+    }
 
 
+  });
 
-    // correct letter pass in. event listener will sit on the li's
-    // look into event delegation which is putting an event listener across the whole UL on each LI
-    // $e.target html
+  // hiding letters when clicked in result_list and show in letters_list
+  $result.on('click', 'li', (e) => {
+    const dataIndex = $(e.target).data('index');
+    $letters.children().filter((index) => {
+      return index === dataIndex;
+    }).show();
+    $(e.target).remove();
 
-  }
+    // use the data index to display the particular LI
+    // display none the LI we just clicked to move back down
+    console.log($(e.target));
+  });
 });
 
 
